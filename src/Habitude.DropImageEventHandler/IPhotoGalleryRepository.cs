0e27f499eb03e0f2a400734c343bf0a0fb56b914
Kritul Rathod <kritulrathod@gmail.com>
2019-12-26 16:50:48 -0500

    Adding Dummy implementation for Repository (for visualizing walking skeleton)

---
+++ src/Habitude.DropImageEventHandler/IPhotoGalleryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habitude.DropImageEventHandler
+{
+  public interface IPhotoGalleryRepository
+  {
+  }
+}
---
+++ src/Habitude.DropImageEventHandler/PhotoGalleryRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habitude.DropImageEventHandler
+{
+  public class PhotoGalleryRepository : IPhotoGalleryRepository
+  {
+  }
+}
