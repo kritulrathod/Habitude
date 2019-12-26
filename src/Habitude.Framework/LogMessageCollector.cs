b1c746061e41724b3f1200b29105f71b2ca94c58
Kritul Rathod <kritulrathod@gmail.com>
2019-12-26 16:49:42 -0500

    Adding dummy in memory Message Logger for debugging - should be scrapped later.

---
+++ src/Habitude.Framework/LogMessageCollector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Habitude.Framework
+{
+  public class LogMessageCollector
+  {
+    private static LogMessageCollector logMessage;
+    private static StringBuilder _builder;
+
+    static LogMessageCollector()
+    {
+      _builder = new StringBuilder();
+    }
+    public static LogMessageCollector Instance
+    {
+      get
+      {
+        if (logMessage != null) logMessage = new LogMessageCollector();
+        return logMessage;
+      }
+    }
+
+    public static void Append(string message)
+    {
+      _builder.AppendLine(message);
+    }
+
+    public static string Flush()
+    {
+      return _builder.ToString();
+    }
+  }
+}
+
