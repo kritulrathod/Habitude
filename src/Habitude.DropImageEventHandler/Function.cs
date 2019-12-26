0558c1f7e866a3fa069e32da6f3bab53a4ed54db
Kritul Rathod <kritulrathod@gmail.com>
2019-12-26 17:10:23 -0500

    Using dummy logger for debugging.

--- src/Habitude.DropImageEventHandler/Function.cs
+++ src/Habitude.DropImageEventHandler/Function.cs
@@ -8,61 +8,77 @@ using Amazon.Lambda.S3Events;
 using Amazon.S3;
 using Amazon.S3.Util;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
-[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+using Habitude.Framework;
+//using Microsoft.Extensions.DependencyInjection;
 
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 namespace Habitude.DropImageEventHandler
 {
-    public class Function
+  public class Function
+  {
+    IAmazonS3 S3Client { get; set; }
+
+    //public static readonly IServiceProvider Container = new ContainerBuilder().Build();
+    //private IDropImageEventProcessor _processor;
+
+    /// <summary>
+    /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
+    /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
+    /// region the Lambda function is executed in.
+    /// </summary>
+    public Function()
     {
-        IAmazonS3 S3Client { get; set; }
+      S3Client = new AmazonS3Client();
+      LogMessageCollector.Append("Function(): Created S3Client");
 
-        /// <summary>
-        /// Default constructor. This constructor is used by Lambda to construct the instance. When invoked in a Lambda environment
-        /// the AWS credentials will come from the IAM role associated with the function and the AWS region will be set to the
-        /// region the Lambda function is executed in.
-        /// </summary>
-        public Function()
-        {
-            S3Client = new AmazonS3Client();
-        }
-
-        /// <summary>
-        /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
-        /// </summary>
-        /// <param name="s3Client"></param>
-        public Function(IAmazonS3 s3Client)
-        {
-            this.S3Client = s3Client;
-        }
-        
-        /// <summary>
-        /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
-        /// to respond to S3 notifications.
-        /// </summary>
-        /// <param name="evnt"></param>
-        /// <param name="context"></param>
-        /// <returns></returns>
-        public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
-        {
-            var s3Event = evnt.Records?[0].S3;
-            if(s3Event == null)
-            {
-                return null;
-            }
-
-            try
-            {
-                var response = await this.S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
-                return response.Headers.ContentType;
-            }
-            catch(Exception e)
-            {
-                context.Logger.LogLine($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
-                context.Logger.LogLine(e.Message);
-                context.Logger.LogLine(e.StackTrace);
-                throw;
-            }
-        }
+      //_processor = Container.GetService<IDropImageEventProcessor>();
+      //LogMessageCollector.Append("Function(): Resolving IDropImageEventProcessor from DI");
     }
+
+    /// <summary>
+    /// Constructs an instance with a preconfigured S3 client. This can be used for testing the outside of the Lambda environment.
+    /// </summary>
+    /// <param name="s3Client"></param>
+    public Function(IAmazonS3 s3Client)
+    {
+      this.S3Client = s3Client;
+      LogMessageCollector.Append("Function(IAmazonS3): using injected S3Client");
+
+      //_processor = Container.GetService<IDropImageEventProcessor>();
+      //LogMessageCollector.Append("Function(IAmazonS3): Resolving IDropImageEventProcessor from DI");
+    }
+
+    /// <summary>
+    /// This method is called for every Lambda invocation. This method takes in an S3 event object and can be used 
+    /// to respond to S3 notifications.
+    /// </summary>
+    /// <param name="evnt"></param>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    public async Task<string> FunctionHandler(S3Event evnt, ILambdaContext context)
+    {
+      var s3Event = evnt.Records?[0].S3;
+      if (s3Event == null)
+      {
+        return null;
+      }
+
+      try
+      {
+        var response = await this.S3Client.GetObjectMetadataAsync(s3Event.Bucket.Name, s3Event.Object.Key);
+
+        //_processor.Process();
+
+        var result = LogMessageCollector.Flush();
+        return response.Headers.ContentType;
+      }
+      catch (Exception e)
+      {
+        context.Logger.LogLine($"Error getting object {s3Event.Object.Key} from bucket {s3Event.Bucket.Name}. Make sure they exist and your bucket is in the same region as this function.");
+        context.Logger.LogLine(e.Message);
+        context.Logger.LogLine(e.StackTrace);
+        throw;
+      }
+    }
+  }
 }
