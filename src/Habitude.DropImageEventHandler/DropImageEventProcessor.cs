330a2a1332d7f170316e9f7ff8b0d677a54729ea
Kritul Rathod <kritulrathod@gmail.com>
2019-12-27 17:56:49 -0500

    Using Framework Implementation of AWS S3 Service to  extract out the dependency on the AWS SDK in Handler

--- src/Habitude.DropImageEventHandler/DropImageEventProcessor.cs
+++ src/Habitude.DropImageEventHandler/DropImageEventProcessor.cs
@@ -2,16 +2,23 @@
 using System.Collections.Generic;
 using System.Text;
 using Habitude.Framework;
+using Habitude.Infrastructure.AWS.S3;
 
 namespace Habitude.DropImageEventHandler
 {
   public class DropImageEventProcessor : IDropImageEventProcessor
   {
+    private readonly IS3Client _s3Client;
     private readonly IPhotoGalleryRepository _photoGalleryRepository;
 
-    public DropImageEventProcessor(IPhotoGalleryRepository photoGalleryRepository)
+    public DropImageEventProcessor(IPhotoGalleryRepository photoGalleryRepository, IS3Client s3Client)
     {
+      _s3Client = s3Client;
       _photoGalleryRepository = photoGalleryRepository;
+    }
+
+    public async void GetObject()
+    {
     }
 
     public void Process()
